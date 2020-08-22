cb51ab6bf915f9cd0acab1c459122b672631846d
DiegoGaby DIEGO <diegogabydiego@macbook-pro-de-diegogaby.home>
2020-08-22 23:23:17 +0200

    layout

--- laravel/resources/views/layout.blade.php
+++ laravel/resources/views/layout.blade.php
@@ -1,20 +1,24 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <title> @yield('title') </title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 </head>
+
 <body>
     <nav>
         <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/contact">Contact</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/portfolio">Portfolio</a></li>
+            <li><a href="{{ route('home') }}">Home</a></li>
+            <li><a href="{{ route('contact') }}">Contact</a></li>
+            <li><a href="{{ route('portfolio') }}">Portfolio</a></li>
+            <li><a href="{{ route('about') }}">About</a></li>
+
         </ul>
     </nav>
     @yield('content')
 </body>
+
 </html>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -18,4 +18,4 @@ Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
 Route::view('/contact', 'contact')->name('contact');
 
-Route::get('/portfolio', 'PortfolioController@index');
+Route::get('/portfolio', 'PortfolioController@index')->name('portfolio');
