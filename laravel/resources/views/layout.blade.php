b6c1a83490a213daa82cc180f2a82af2c03e68d0
DiegoGaby DIEGO <diegogabydiego@macbook-pro-de-diegogaby.home>
2020-08-29 20:44:33 +0200

    Link et class active

--- laravel/resources/views/layout.blade.php
+++ laravel/resources/views/layout.blade.php
@@ -6,19 +6,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
 </head>
 
 <body>
     <nav>
         <ul>
-            <li><a href="{{ route('home') }}">Home</a></li>
-            <li><a href="{{ route('contact') }}">Contact</a></li>
-            <li><a href="{{ route('portfolio') }}">Portfolio</a></li>
-            <li><a href="{{ route('about') }}">About</a></li>
+            <li class=" {{request()->routeIs('home') ? 'active' : ''}}"><a href="{{ route('home') }}">Home</a></li>
+            <li class=" {{request()->routeIs('contact') ? 'active' : ''}}"><a href="{{ route('contact') }}">Contact</a></li>
+            <li class=" {{request()->routeIs('portfolio') ? 'active' : ''}}"><a href="{{ route('portfolio') }}">Portfolio</a></li>
+            <li class=" {{request()->routeIs('about') ? 'active' : ''}}"><a href="{{ route('about') }}">About</a></li>
 
         </ul>
     </nav>
     @yield('content')
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 
 </html>
