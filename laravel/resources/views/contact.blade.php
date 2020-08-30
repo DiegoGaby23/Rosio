e7e20c8bf7d76eec975b9285228b3fa8b8528719
DiegoGaby DIEGO <diegogabydiego@macbook-pro-de-diegogaby.home>
2020-08-30 17:13:27 +0200

    Validar formularios

--- laravel/resources/views/contact.blade.php
+++ laravel/resources/views/contact.blade.php
@@ -1,7 +1,27 @@
 @extends('layout')
 
 @section('title', 'Contact')
-    
+
 @section('content')
-    <h1>Contact</h1>
+<h1>Contact</h1>
+
+
+{{-- FORM VALIDATE --}}
+<form method="POST" action=" {{ route('contact') }} ">
+    @csrf
+    <input name="name" placeholder="Nombre..." value="{{old ('name') }}"><br>
+    {{ $errors->first('name') }}<br>
+
+    <input type="email" placeholder="E-mail..." value="{{old ('email') }}"><br>
+    {{ $errors->first('email') }}<br>
+
+    <input name="subject" placeholder="Asunto..." value="{{ old('subject') }}"><br>
+    {{ $errors->first('subject') }}<br>
+
+    <textarea name="content" placeholder="Mensage  ..." value="{{old ('content') }}"></textarea><br>
+    {{ $errors->first('content') }}<br>
+
+    <button>Enviar</button><br>
+</form>
+
 @endsection
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -17,5 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
 Route::view('/contact', 'contact')->name('contact');
+Route::post('contact', 'MessagesController@store');
 
 Route::get('/portfolio', 'PortfolioController@index')->name('portfolio');
